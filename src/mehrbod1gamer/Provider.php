5f778a2793622b755a5135a86bff67b2289a81a5
mehrbod1gamer <72642714+mehrbod1gamer@users.noreply.github.com>
2021-01-28 11:13:25 +0330

    Create Provider.php

---
+++ src/mehrbod1gamer/Provider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mehrbod1gamer;
+
+use pocketmine\Player;
+
+interface Provider
+{
+    public function getCoin(Player $player) : int;
+    public function reduceCoin(Player $player, int $count) : void;
+    public function isRegister(Player $player) : bool;
+    public function giveCoin(Player $player, int $count) : void;
+}
