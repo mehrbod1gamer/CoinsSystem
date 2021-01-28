dd31af89d1c2d5956e4ca5678cc6b446a71ac4ce
mehrbod1gamer <72642714+mehrbod1gamer@users.noreply.github.com>
2021-01-28 11:46:30 +0330

    Update EconomyAPI.php

--- src/mehrbod1gamer/Lib/economyapi/EconomyAPI.php
+++ src/mehrbod1gamer/Lib/economyapi/EconomyAPI.php
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace onebone\economyapi;
+namespace mehrbod1gamer\Lib\economyapi;
 
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
